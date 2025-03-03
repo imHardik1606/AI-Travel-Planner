8c18a280a14ea87ee4674ca8a01ac9f87e311d9c
Hardik Gayner <hardikgayner@gmail.com>
2025-03-03 18:18:04 +0530

    Viewing trip details

--- frontend/src/view-trip/components/PlaceCardItem.jsx
+++ frontend/src/view-trip/components/PlaceCardItem.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { FaMapLocationDot } from "react-icons/fa6";
 import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
-function PlaceCardItem({place}) {
+function PlaceCardItem({ place }) {
   return (
-    <div className='border rounded-xl p-3 mt-2 flex gap-4'>
-        <img src="/trip.jpg" alt="" className='w-[150px] h-[150px] rounded-xl'/>
-        <div>
-            <h2 className='font-bold text-lg'>{place.placeName}</h2>
-            <p className='text-sm text-gray-400'>{place.placeDetails}</p>
-            <h2 className='mt-2 text-sm'>🕙{place.travelTime}</h2>
-            <Button><FaMapLocationDot /></Button>
-            
+    <Link to={"https://www.google.com/maps/search/?api=1&query=" + place?.placeName} target='_blank'>
+      <div className='border rounded-xl p-3 mt-2 flex flex-col md:flex-row gap-4 hover:scale-105 shadow-md transition-all cursor-pointer'>
+        {/* Image Section */}
+        <img src="/trip.jpg" alt="" className='w-full md:w-[150px] h-[150px] rounded-xl object-cover' />
+        
+        {/* Text Section */}
+        <div className='w-full md:w-auto text-center md:text-left'>
+          <h2 className='font-bold text-lg'>{place.placeName}</h2>
+          <p className='text-sm text-gray-400'>{place.placeDetails}</p>
+          <h2 className='mt-2 text-sm'>🕙{place.travelTime}</h2>
+          {/* <Button size="sm" className="mt-3"><FaMapLocationDot /></Button> */}
         </div>
-    </div>
-  )
+      </div>
+    </Link>
+  );
 }
 
-export default PlaceCardItem
+export default PlaceCardItem;
