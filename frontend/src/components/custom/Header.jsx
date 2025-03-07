441bcf5b807f1ab65da8358ae977b4d4c5ce66b6
Hardik Gayner <hardikgayner@gmail.com>
2025-03-07 19:34:21 +0530

    Added user icon & logout btn

--- frontend/src/components/custom/Header.jsx
+++ frontend/src/components/custom/Header.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
-import { Button } from '../ui/button'
+import React, { useEffect } from "react";
+import { Button } from "../ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { googleLogout } from "@react-oauth/google";
+import { useNavigation } from "react-router-dom";
+
 
 function Header() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    console.log(user);
+  }, []);
+
   return (
-    <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-        <img src="/logo.svg" alt="" />
-        <div>
-            <Button>Sign In</Button>
-        </div>
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
+      <img src="/logo.svg" alt="" />
+      <div>
+        {user ? (
+          <div className="flex items-center gap-5">
+            <Button variant="outline" className="rounded-full">
+              My Trips
+            </Button>
+            
+            <Popover>
+              <PopoverTrigger><img
+              src={user ? user?.picture : "/userFallback.jpg"}
+              className="h-10 w-10 rounded-full"
+            /></PopoverTrigger>
+              <PopoverContent>
+                <h2 onClick={() => {
+                  googleLogout();
+                  localStorage.clear();
+                  window.location.reload();
+                }} className="cursor-pointer">Logout</h2>
+              </PopoverContent>
+            </Popover>
+          </div>
+        ) : (
+          <Button>Sign In</Button>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
