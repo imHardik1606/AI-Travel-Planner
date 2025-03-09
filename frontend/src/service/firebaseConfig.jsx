30dbc923b6bbd4db49bf3efc6ec001980127ac1a
Hardik Gayner <hardikgayner@gmail.com>
2025-03-09 19:07:09 +0530

    made landing & create-trip page responsive

--- frontend/src/service/firebaseConfig.jsx
+++ frontend/src/service/firebaseConfig.jsx
@@ -8,7 +8,7 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-I1QvwLBZlJ3GhrvJvghLsbIz1Glzifk",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "ai-projects-80e0d.firebaseapp.com",
   projectId: "ai-projects-80e0d",
   storageBucket: "ai-projects-80e0d.firebasestorage.app",
